24b56a35da7463887b26acf6b551d341867d4112
Ludovico Fischer <livrerie@gmail.com>
2020-12-09 20:54:49 +0100

    add test for processor plugins.

--- tests/processors.js
+++ tests/processors.js
@@ -1,4 +1,4 @@
-const { test } = require('uvu');
+const { suite } = require('uvu');
 const assert = require('assert').strict;
 const postcss = require('postcss');
 
@@ -32,23 +32,65 @@ function colorToBorder() {
 }
 colorToBorder.postcss = true;
 
-test('shall convert color and border with separate processor', () => {
-  return postcss([colorToBorder, postcss([black])])
-    .process(css, { from: undefined })
-    .then((result) => {
+const givenProcessorPlugin = suite('given the plugin is a processor');
+givenProcessorPlugin(
+  'shall convert color and border with separate processor',
+  () => {
+    return postcss([colorToBorder, postcss([black])])
+      .process(css, { from: undefined })
+      .then((result) => {
+        assert.strictEqual(result.css, expected);
+      });
+  }
+);
+
+givenProcessorPlugin.run();
+
+const givenPluginReturnsProcessor = suite(
+  'given the plugin returns a processor'
+);
+
+givenPluginReturnsProcessor(
+  'shall apply the plugin together with others',
+  () => {
+    function createPlugin() {
+      return postcss([black]);
+    }
+    createPlugin.postcss = true;
+    const processor = postcss([colorToBorder, createPlugin]);
+    return processor.process(css, { from: undefined }).then((result) => {
       assert.strictEqual(result.css, expected);
     });
-});
-
-test('shall convert color and border with strange plugin', () => {
-  function createPlugin() {
-    return postcss([black]);
   }
-  createPlugin.postcss = true;
-  const processor = postcss([colorToBorder, createPlugin]);
+);
+
+givenPluginReturnsProcessor('the plugin shall remove nodes', () => {
+  let plugin = () => {
+    return {
+      postcssPlugin: 'declaration-remover',
+      Declaration: {
+        color: (decl) => {
+          decl.remove();
+        },
+      },
+    };
+  };
+  plugin.postcss = true;
+
+  let meta = () => {
+    return postcss([plugin]);
+  };
+  meta.postcss = true;
+
+  const processor = postcss([meta]);
   return processor.process(css, { from: undefined }).then((result) => {
-    assert.strictEqual(result.css, expected);
+    assert.strictEqual(
+      result.css,
+      `
+p {
+}`
+    );
   });
 });
 
-test.run();
+givenPluginReturnsProcessor.run();
